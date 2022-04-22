f97ea76aa503577f2d0c39d419fba10894bccb77
Diego Artiles <diego@dartiles.dev>
2022-04-22 16:25:54 -0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
             <img src="../assets/logo.png" alt="logo" class="header__logo" />
         </div>
         <div class="header__banner-text-container">
-            <p class="header__banner-text">Envía bienestar a tu familia en <b class="header__banner-text-bold">Venezuela</b></p>
+            <p class="header__banner-text">Envía bienestar a tu familia en <b class="header__banner-text-bold">Argentina</b></p>
         </div>
         <div class="header__banner-image-container">
         </div>
